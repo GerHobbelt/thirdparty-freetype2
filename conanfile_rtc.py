d6b59ceebd2de3892293ae6760436fd3f109d46a
Ivan Trombley <itrombley@esri.com>
2021-06-10 15:48:32 -0700

    Added

---
+++ conanfile_rtc.py
@@ -0,0 +1,23 @@
+from conans import ConanFile
+
+
+class FreeTypeConan(ConanFile):
+    name = "freetype"
+    version = "2.10.4"
+    url = "https://github.com/Esri/freetype/tree/runtimecore"
+    license = "https://github.com/Esri/freetype/blob/runtimecore/docs/FTL.TXT"
+    description = "FreeType is a freely available software library to render fonts."
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/freetype/"
+
+        # headers
+        self.copy("*.h", src=base + "include", dst=relative + "include")
+
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
